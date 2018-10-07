5b01c1b1a278fa2f74ad8978edaf14ccf86938fe
Ramvardhan <42577719+Ram-the-coder@users.noreply.github.com>
2018-10-07 16:34:22 +0530

    Improved UI of Leaderboard
    - Added Colors
    - Removed "View" button and instead made the whole record clickable - makes it easier to view and also looks nice.
    - Replaced table with Divs and implemented grids for alignment

--- src/components/RecordItems.js
+++ src/components/RecordItems.js
@@ -1,8 +1,9 @@
- import React from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import {
     Card, CardText, CardBody,
-    CardTitle
+    CardTitle,
+    Progress
 } from 'reactstrap';
 import './RecordItems.css'
 import goldMedal from '../assets/gold-medal.svg'
@@ -23,6 +24,7 @@ function importAll(r) {
 
 const boys = importAll(require.context('../assets/boys', false, /\.(png|jpe?g|svg)$/));
 const girls = importAll(require.context('../assets/girls', false, /\.(png|jpe?g|svg)$/));
+
 
 class ListRecord extends React.Component {
     constructor(props) {
@@ -50,11 +52,13 @@ class ListRecord extends React.Component {
 
     render() {
         let toppers = [];
+        let maxPoints=0; //This will hold the points of the 1st ranker
         for (var i = 0; i < ((this.props.records.Reducer.records.length > 3) ? 3 : this.props.records.Reducer.records.length); i++) {
+          maxPoints=this.props.records.Reducer.records[0]["points"]; //Updating the maxPoints to hold the points of the 1st ranker
             if(this.state.search === ''  || (this.props.records.Reducer.records[i]["name"]["first"].toLowerCase().includes(this.state.search.toLowerCase())) ||  (this.props.records.Reducer.records[i]["name"]["last"].toLowerCase().includes(this.state.search.toLowerCase()))) {
                 toppers.push(
                     <Col sm="12" md={{size: 4}} key={i}>
-                        <Card className='h-100'>
+                        <Card className='h-100 hover' id={i} onClick={this.createView.bind(this, this.props.records.Reducer.records[i], i)}>
                             <div className="text-center">
 
                                 <img src={this.props.records.Reducer.records[i]["photo"]["type"] === 'boys' ? boys[this.props.records.Reducer.records[i]["photo"]["number"] + '.svg'] : girls[this.props.records.Reducer.records[i]["photo"]["number"] + '.svg']}
@@ -66,14 +70,12 @@ class ListRecord extends React.Component {
                                 {i === 0 ? <div className='text-center'><img alt='medal' src={goldMedal} width='40'/></div> : ''}
                                 {i === 1 ? <div className='text-center'><img alt='medal' src={silverMedal} width='40'/></div> : ''}
                                 {i === 2 ? <div className='text-center'><img alt='medal' src={bronzeMedal} width='40'/></div> : ''}
-                                <hr className='hr'/>
+                                <br />
+                                <div className="progressBar">
+                                  <Progress striped  value={this.props.records.Reducer.records[i]["points"]} max={maxPoints} />
+                                </div>
                                 <CardText
                                     className='cardTitle cardPoint'>{this.props.records.Reducer.records[i]["points"]}</CardText>
-                                <div className='text-center'>
-                                    <button className='buttonView btn btn-link'
-                                            onClick={this.createView.bind(this, this.props.records.Reducer.records[i], i)}> View
-                                    </button>
-                                </div>
                             </CardBody>
                         </Card>
                     </Col>)
@@ -84,20 +86,17 @@ class ListRecord extends React.Component {
             if(this.state.search === ''  || (this.props.records.Reducer.records[i]["name"]["first"].toLowerCase().includes(this.state.search.toLowerCase())) ||  (this.props.records.Reducer.records[i]["name"]["last"].toLowerCase().includes(this.state.search.toLowerCase()))) {
 
                 others.push(
-                    <tr key={i}>
-                        <th className='cardTitle th cardNumber' scope="row">{i + 1}</th>
-                        <td className='td'><img className="rounded-circle align-self-start mr-3"
-                                                src={this.props.records.Reducer.records[i]["photo"]["type"] === 'boys' ? boys[this.props.records.Reducer.records[i]["photo"]["number"] + '.svg'] : girls[this.props.records.Reducer.records[i]["photo"]["number"] + '.svg']}
-                                                alt="Generic placeholde"
-                                                width='50px' height='50px'/></td>
-                        <td className='td cardTitle'>{this.props.records.Reducer.records[i]["name"]["first"]} <span className='teamId'> ( Team ID : {this.props.records.Reducer.records[i]["teamId"]} ) </span></td>
-                        <td className='td'>{this.props.records.Reducer.records[i]["points"]}</td>
-                        <td className='td'>
-                            <button className='buttonView btn btn-link'
-                                    onClick={this.createView.bind(this, this.props.records.Reducer.records[i], i)}> View
-                            </button>
-                        </td>
-                    </tr>
+                      <Row className="list hover" onClick={this.createView.bind(this, this.props.records.Reducer.records[i], i)}>
+                        <Col className="">
+                          <img src={this.props.records.Reducer.records[i]["photo"]["type"] === 'boys' ? boys[this.props.records.Reducer.records[i]["photo"]["number"] + '.svg'] : girls[this.props.records.Reducer.records[i]["photo"]["number"] + '.svg']} height="80px" class="rounded-circle align-self-start mr-3" />
+                        </Col>
+                        <Col className="list-text">
+                          <h5>{i}. {this.props.records.Reducer.records[i]["name"]["first"]}</h5> ( Team ID : {this.props.records.Reducer.records[i]["teamId"]} )
+                        </Col>
+                        <Col className="list-text">
+                          <h5 className="list-text">{this.props.records.Reducer.records[i]["points"]}</h5>
+                        </Col>
+                      </Row>
                 )
             }
         }
@@ -131,30 +130,23 @@ class ListRecord extends React.Component {
                             {toppers}
                         </Row>
                         <Row className='otherRow'>
-                            <Table borderless='true'>
-                                <tbody>
+                                <Container>
                                 {others}
-                                </tbody>
-                            </Table>
+                              </Container>
                         </Row>
                     </Container>
                 </Col>
                 {this.state.size === 8 ? (
-                    <Col sm="12" md={{size: 4}} className='sizeClass'>
+                    <Col sm="12" md={{size: 4}} className='sizeClass' id={this.state.position}>
                         <Button color="link buttonView" onClick={this.closeIt}>Close</Button>
                         <div className="text-center">
                             <img src={this.state.person["photo"]["type"] === 'boys' ? boys[this.state.person["photo"]["number"] + '.svg'] : girls[this.state.person["photo"]["number"] + '.svg']} className="img-fluid rounded-circle view" alt="..."/>
                         </div>
                         <CardTitle
                             className='cardTitle'>{this.state.person["name"]["first"]} {this.state.person["name"]["last"]}</CardTitle>
-
-                        {this.state.position === 0 ?
-                            <div className='text-center'><img  alt='medal' src={goldMedal} width='40'/></div> : ''}
-                        {this.state.position === 1 ?
-                            <div className='text-center'><img alt='medal' src={silverMedal} width='40'/></div> : ''}
-                        {this.state.position === 2 ?
-                            <div className='text-center'><img alt='medal' src={bronzeMedal} width='40'/></div> : ''}
-                        <hr className='hr view'/>
+                        <div className="progressBar">
+                          <Progress striped  value={this.state.person["points"]} max={maxPoints} />
+                        </div>
                         <CardText className='cardTitle cardPoint'>{this.state.person["points"]}</CardText>
                         <Container>
                             <Row className='Social'>
@@ -191,6 +183,7 @@ class ListRecord extends React.Component {
                             <h5><b> Team Id</b> : {this.state.person["teamId"]}</h5>
                             <h5><b> Year of Study</b> : {this.state.person["year"]}</h5>
                             <h5> <b> About </b>: {this.state.person["about"]}</h5>
+
                         </Container>
                         <Container> {/* Implementing QR Code here. It will show Name, TeamID, Year of Study, Points, Github Link, LinkedIn Link and Email of the user */}
                           <center>
