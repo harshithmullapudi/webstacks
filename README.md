352405f296d9383a223c80bb6bfeb59325619a2d
harshithmullapudi <harshithmullapudi@gmail.com>
2018-10-07 14:05:58 +0530

    Changed the question ui with some validation

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-#React - webstacks
+# React - webstacks
 
 ## A website designed for the club (Web development) to make learning process much easier and also competitive.
 
-###Features
+### Features
 1. Leaderboard
 2. Forum
 3. Profile
 4. Task View and Task submission
 
 
-###Contributions
+### Contributions
 
 1. [ Aravind Srinivasan ](https://github.com/Arav1nd2) -  QrCode feature 
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,14 @@
 import firebase from 'firebase'
 
 
-const config = process.env.config
+const config = {
+    "apiKey": "AIzaSyDSr3xqO6BtDPfjD8k5QF6VcpXs1VKLRs4",
+    "authDomain": "myapp-a123c.firebaseapp.com",
+    "databaseURL": "https://myapp-a123c.firebaseio.com",
+    "projectId": "myapp-a123c",
+    "storageBucket": "myapp-a123c.appspot.com",
+    "messagingSenderId": "780709071980"
+};
 firebase.initializeApp(config);
 
 export const database = firebase.database()
