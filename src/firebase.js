f9db948ef7bf8bccfe6c9346ee478057b1029126
harshithmullapudi <harshithmullapudi@gmail.com>
2018-10-07 13:36:45 +0530

    Changed the question ui with some validation

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase'
 
 
-const config = process.env.firebase;
+const config = JSON.parse(process.env.firebase);
 firebase.initializeApp(config);
 
 export const database = firebase.database()
