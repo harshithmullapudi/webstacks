10d259772f2f2c7746baf3b21d1fa104a45aea33
Ramvardhan <42577719+Ram-the-coder@users.noreply.github.com>
2018-10-13 23:04:28 +0530

    Final task - Stage 1

---
+++ src/components/chat.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+export default class Chat extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return(
+      <p>Hello chat component</p>
+    );
+  }
+}
