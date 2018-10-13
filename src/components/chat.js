83b0b96360e0f27e0168710968a0e48f1b91a98e
Vashanth <43083250+Vashanth@users.noreply.github.com>
2018-10-13 23:06:53 +0530

    Add files via upload

---
+++ src/components/chat.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './chat.css'; 
+class Chatview extends Component {
+
+  render() {
+  	
+      return (
+        <h1 className="head1">CHATTING COMPONENT</h1>
+    );
+  }
+}
+
+export default Chatview;
