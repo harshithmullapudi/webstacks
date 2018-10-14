a9d7409ed8fff906fc585ea3cc690f5045fc656d
Ramvardhan <42577719+Ram-the-coder@users.noreply.github.com>
2018-10-14 19:27:06 +0530

    Improved UI

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import './chat.css';
-import {Row,Col,Button,InputGroup, Input} from 'reactstrap';
 const DUMMY_DATA = [
+  {
+    senderId: "Lincoln",
+    text: "Hi everyone!"
+  },
+  {
+    senderId: "Abraham",
+    text: "Hi Lincoln!"
+  },
+  {
+    senderId: "Obama",
+    text: "Hi everyone!"
+  },
+  {
+    senderId: "Ram",
+    text:"This is some long long text. Lorem ipsum door set amet dolor wolf harry potter lady lestrange. This is some long long text. Lorem ipsum door set amet dolor wolf harry potter lady lestrange. This is some long long text. Lorem ipsum door set amet dolor wolf harry potter lady lestrange"
+  },
   {
     senderId: "Lincoln",
     text: "Hi everyone!"
@@ -24,32 +39,49 @@ export default class Chat extends Component {
   }
   render() {
     return(
-      <div class="container">
-        <h3>Webstacks Chat Room</h3>
-        {/* Message List */}
-        <div>
-          {this.state.messages.map( message => {
-            return(
-              <div key={message.id}>
-                <div className="message-sender">
-                  {message.senderId}
-                </div>
-                <div className="message-text-container">
-                  <span className="message-text">{message.text}</span>
-                </div>
-              </div>
-            )
-          }
-          )}
+      <div className="chat-component">
+      <div className="chat-room">
+        <div className="sidebar">
+          Information like users online can be displayed here...
         </div>
-        {/* Chat message submit form*/}
-        <div className="send-message">
-          <InputGroup>
-            <Input placeholder="Enter message to send" />
-              <Button>Send</Button>
-          </InputGroup>
+        <div className="chat-block">
+          <div className="chat-block-header">
+              <b>Common Chat Room </b>
+              <br />
+              Webstacks
+          </div>
+          <div className="chat-block-body">
+            <div className="message-block">
+              {this.state.messages.map( message => {
+                return(
+                  <div key={message.id}>
+                    <div className="message-sender">
+                      {message.senderId}
+                    </div>
+                    <div className="message-text-container">
+                      <span className="message-text">{message.text}</span>
+                    </div>
+                  </div>
+                )
+              }
+              )}
+            </div>
+            <div className="send-message-box">
+                <div className="send-message-box-input">
+                    <form id="send-message-form">
+                        <input
+                            className="send-message-box-input"
+                            id="send-message-box-input"
+                            type="text"
+                            placeholder="Send message"
+                        />
+                    </form>
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      </div>
     );
   }
 }
