385e641b604e57467b4366263a06b7067b6d1ce7
Vashanth <43083250+Vashanth@users.noreply.github.com>
2018-10-14 23:42:14 +0530

    Add files via upload

--- src/components/chat.js
+++ src/components/chat.js
@@ -7,30 +7,32 @@ class Chatview extends Component {
   	
       return (
         <div className="head1"><h1>CHAT ROOM</h1>
-        <div className="chatContainer"><div className="chat1"><p>Vashanth:Hi!</p></div></div>
-        <div className="chatContainer"><div className="chat2"><p>Ram:Hi!</p></div></div>
-        <div className="chatContainer"><div className="chat1"><p>Vashanth:How are you?</p></div></div>
-        <div className="chatContainer"><div className="chat2"><p>Ram:I'm fine</p></div></div>
 
-        <div className="chatContainer"><div className="chat1"><p>Vashanth:How's college?</p></div></div>
-        <div className="chatContainer"><div className="chat2"><p>Ram:Good!</p></div></div>
-        <div className="chatContainer"><div className="chat1"><p>Vashanth:Okay</p></div></div>
+        <div className="right">
+        <div class="temp"><span className="chat1"><p>Vashanth:Hi!</p></span></div>
+        <div class="temp"><span className="chat1"><p>Ram:Hi!</p></span></div>
+        <div class="temp"><span className="chat1"><p>Vashanth:How are you?</p></span></div>
+        <div class="temp"> <span className="chat1"><p>Ram:I'm fine</p></span></div>
 
-
-
-
+        <div class="temp"><span className="chat1"><p>Vashanth:How's college?</p></span></div>
+        <div class="temp"><span className="chat1"><p>Ram:Good!</p></span></div>
+        <div class="temp"><span className="chat1"><p>Vashanth:Okay</p></span></div>
+        <div class="temp"><span className="chat1"><p>Ram:There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...</p></span></div>
+        
         <div class="chatContainers">
-        <div class="row">
-   <div class="col-lg-12">
-    <div class="input-group input-group-lg">
-      <input type="text" class="form-control input" id="search-church" placeholder="Send message:" />
-      <pre> </pre><span class="input-group-btn">
-        <button class="btn btn-success btn-lg" type="submit">Send</button>
-      </span>
-    </div>
-  </div>
+      <input type="text" class="chatContainers" id="chats" placeholder="send msg:" name="chats" />
 </div>
         </div>
+
+        <div className="left">
+        <ul class="list-group">
+    <li class="list-group-item active">Members in chat</li>
+    <li class="list-group-item">Vashanth</li>
+    <li class="list-group-item">Ram</li>
+  </ul>
+        </div>
+
+
         </div>
     );
   }
