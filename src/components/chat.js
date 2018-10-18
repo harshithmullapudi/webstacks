fd3429b8a6c4c025f575ff8bf133fee86e260f55
Ramvardhan <42577719+Ram-the-coder@users.noreply.github.com>
2018-10-19 01:02:18 +0530

    Adding file upload feature

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import {Progress} from 'reactstrap';
 import './chat.css';
 import * as firebase from '../firebase';
-import {database, auth} from '../firebase';
+import FileUploader from "react-firebase-file-uploader";
+import {database, storage} from '../firebase';
 import attach from '../assets/paperclip.png';
 import emoticon from '../assets/smile.png';
+import send from '../assets/send.png';
 export default class Chat extends Component {
   constructor() {
     super();
@@ -13,6 +16,11 @@ export default class Chat extends Component {
     this.state = {
        messages: [],
        newMessage: "",
+       uploadedFile: "",
+       isUploading: false,
+       uploadProgress: 0,
+       fileURL: "",
+       wantToUpload: false,
     }
   }
 
@@ -23,7 +31,9 @@ export default class Chat extends Component {
       previousMessages.push({
         id: snap.key,
         senderId: snap.val().senderId,
-        text: snap.val().text
+        text: snap.val().text,
+        file: snap.val().file,
+        file_url: snap.val().file_url
       })
       this.setState({
         messages: previousMessages,
@@ -39,10 +49,39 @@ export default class Chat extends Component {
   sendMessage(event) {
     event.preventDefault();
     //Sends the message typed to the db
-    this.database.push().set({senderId: "Temp", text: this.state.newMessage});
+    this.database.push().set({senderId: "Temp", text: this.state.newMessage, file_name: this.state.uploadedFile, file_url: this.state.fileURL});
+    // this.database.put(this.state.newFiles);
     this.setState({
-      newMessage: ''
+      newMessage: "",
+      uploadedFile: "",
+      isUploading: false,
+      uploadProgress: 0,
+      fileURL: "",
+      wantToUpload: false,
+
     })
+  }
+  showUpload = () => {
+    this.state.wantToUpload ? this.setState({wantToUpload: false}) : this.setState({wantToUpload: true})
+  }
+  handleUploadStart = () => {
+    console.log("Upload started");
+    this.setState({ isUploading: true })
+  }
+  handleUploadError() {
+    console.log("Upload error");
+  }
+  handleUploadSuccess = filename => {
+    console.log("Upload success");
+    this.setState({ uploadedFile: filename, isUploading: false, uploadProgress: 0 });
+    storage
+      .ref("images")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ fileURL: url }));
+  }
+  handleUploadProgress = progress => {
+    this.setState({ uploadProgress: progress })
   }
   render() {
     return(
@@ -67,6 +106,18 @@ export default class Chat extends Component {
                       </div>
                       <div className="message-text-container">
                         <span className="message-text">{message.text}</span>
+                        {
+                          message.file_url != '' ?
+                            <span class="attachment">
+                              <br /><br />
+                              <a href={message.file_url} target="_blank" className="white-text">
+                                <img src={attach} alt="Attachment" className="attachment-in-chat" />
+                                Click to open the attachement
+                              </a>
+                            </span>
+                            : ''
+                        }
+
                       </div>
                     </div>
                   )
@@ -76,8 +127,10 @@ export default class Chat extends Component {
               <div className="send-message-box">
                   <div className="send-message-box-input">
                       <form id="send-message-form" onSubmit={this.sendMessage}>
+                          <img src={attach} alt="Attachment" className="clickable-icon hover-style" onClick={this.showUpload} />
+                          <img src={emoticon} alt="Emoticons" className="clickable-icon hover-style" />
                           <input
-                              className="send-message-box-input"
+                              className="send-box hover-style"
                               id="send-message-box-input"
                               type="text"
                               placeholder="Send message"
@@ -85,8 +138,32 @@ export default class Chat extends Component {
                               onChange={this.handleUserInput}
                               onSubmit={this.sendMessage}
                           />
-                          <img src={attach} alt="Attachment" className="clickable-icon" />
-                          <img src={emoticon} alt="Emoticons" className="clickable-icon" />
+                          <img src={send} alt="Send Message" className="send-icon" onClick={this.sendMessage}/>
+                          {
+                            this.state.wantToUpload ?
+                            <div>
+                              <div id="upload" className="upload">
+                                <FileUploader
+                                  name="file"
+                                  storageRef={storage.ref("images")}
+                                  onUploadStart={this.handleUploadStart}
+                                  onUploadError={this.handleUploadError}
+                                  onUploadSuccess={this.handleUploadSuccess}
+                                  onProgress={this.handleUploadProgress}
+                                />
+                                <a onClick={this.showUpload}>Close</a>
+                                {
+                                  this.state.isUploading ?
+                                    <div className="file-upload-progress">
+                                      <Progress value={this.state.uploadProgress} max="100" />
+                                    </div>
+                                  : ''
+                                }
+
+                              </div>
+                            </div>
+                            : ''
+                          }
                       </form>
                   </div>
               </div>
